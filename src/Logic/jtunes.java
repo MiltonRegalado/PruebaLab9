c7fa83c0adee1f542d081a4b6b860bf5bfb50d7b
MiltonRegalado <140996210+MiltonRegalado@users.noreply.github.com>
2023-12-08 09:07:35 -0600

    Update jtunes.java

--- src/Logic/jtunes.java
+++ src/Logic/jtunes.java
@@ -9,6 +9,24 @@ package Logic;
  * @author suyen
  */
 public class jtunes {
- 
+ private Song song[];
+  
+  public Song SearchSong(int codigo){
+      for(Song buscar:song){
+          if(buscar != null && buscar.getCodigo() == codigo){
+              return buscar;
+          }
+      }
+      return null;
+  }
+  public boolean addSong(int codigo, String nombre, double precio){
+      if(SearchSong(codigo) == null){
+          for(int pos = 0; pos < song.length; pos++){
+              song[pos] = new Song(codigo, nombre, precio);
+          }
+          return true;
+      }
+      return false;
+  }
  
 }
